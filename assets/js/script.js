01de9273ae95fd371bd03a78058d2e13b47d1eb1
Deiontre10 <travontazlowry@gmail.com>
2022-12-05 20:48:55 -0500

    added more functions to script.js, attempted to make quiz stop when time hits 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 var questionEl = document.querySelector(".question");
 var cursor = 0;
 var timeLeft = 80;
+var score = 0;
 var timeEl = document.querySelector("#timer");
 var headerEl = document.querySelector("header");
 var mainEl = document.querySelector("#welcome");
 var startButton = mainEl.querySelector("button");
-var finalScores = document.querySelector("#final");
+var finalScores = document.querySelector("article");
+
 headerEl.style.display = "flex";
 headerEl.style.justifyContent = "space-between";
 
@@ -13,7 +15,7 @@ var questions = [
     {
         text: "How much wood could a woodchuck chuck?",
         correctAnswer: "a",
-        possible : [
+        possible: [
             "a.",
             "b.",
             "c.",
@@ -23,7 +25,7 @@ var questions = [
     {
         text: "Did you have your break today?",
         correctAnswer: "a",
-        possible : [
+        possible: [
             "a.",
             "b.",
             "c.",
@@ -33,7 +35,7 @@ var questions = [
     {
         text: "Do you like ice cream?",
         correctAnswer: "a",
-        possible : [
+        possible: [
             "a.",
             "b.",
             "c.",
@@ -43,7 +45,7 @@ var questions = [
     {
         text: "Whats your favorite pizza topping?",
         correctAnswer: "a",
-        possible : [
+        possible: [
             "a.",
             "b.",
             "c.",
@@ -54,32 +56,32 @@ var questions = [
 
 var correctChoices = ["d", "a", "c", "b",];
 
-var init = function(event) {
-    // event.preventDefault();
+var init = function (event) {
+    event.preventDefault();
     mainEl.style.display = "block";
     questionEl.style.display = "none";
-    finalScores.style.display = "none";
-   
 }
 
-var displayTime = function() {
-  timeEl.textContent = "Time Left: " + timeLeft;
+var displayTime = function () {
+    timeEl.textContent = "Time Left: " + timeLeft;
 }
 
-var setTime = function() {
+var setTime = function () {
     displayTime();
-    var countdown = setInterval(function() {
-      timeLeft--;
-      displayTime();
-  
-      if(timeLeft === 0 || questions.length === 0) {
-        clearInterval(countdown);
-      }
-    }, 1000);
-  }
+    var countdown = setInterval(function () {
+        timeLeft--;
+        displayTime();
 
-var displayQuestion = function() {
+        if (timeLeft === 0) {
+            clearInterval(countdown);
+        }
+        
+    }, 1000);
+}
+
+var displayQuestion = function () {
     mainEl.style.display = "none";
+
     questionEl.querySelector("h2").textContent = questions[cursor].text;
     questionEl.querySelector("#possible").innerHTML = null;
     for (var buttonLabel of questions[cursor].possible) {
@@ -89,34 +91,44 @@ var displayQuestion = function() {
         buttonEl.dataset.choice = buttonLabel[0];
         questionEl.querySelector("#possible").appendChild(buttonEl);
     }
-    highscore();
 }
 
-var advance = function(event) {
+var advance = function (event) {
     var element = event.target;
     if (element.matches(".question button")) {
         var answer = element.dataset.choice === correctChoices[cursor];
-        if (cursor < questions.length - 1) {
+        if (cursor < questions.length + 1) {
             cursor++;
             questionEl.dataset.index = cursor;
+            displayQuestion();
         };
-        displayQuestion();
-        console.log(answer)
-    };
+        if (answer === true) {
+            timeLeft += 10;
+        } else { 
+            if (answer === false || cursor > questions.length + 1) {
+                timeLeft -=10;
+            } else {
+                timeLeft == 0;
+                quizOver();
+                
+            }
+        }
+    }
 };
 
-var highscore = function () {
+var quizOver = function () {
     mainEl.style.display = "none";
     questionEl.style.display = "none";
+    timeEl.textContent = 0;
     finalScores.style.display = "block";
+
 }
 
-questionEl.addEventListener("click", advance)
-startButton.addEventListener("click", function() {
-    setTime()
-    displayQuestion()
+startButton.addEventListener("click", function () {
+    setTime();
+    displayQuestion();
 })
+questionEl.addEventListener("click", advance)
 
 
 // displayQuestion();
-// init();
